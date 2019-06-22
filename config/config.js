6dca2b2d3e318551d0e1d5b8b77ea5296cd00f44
fersoy <46794186+fersoy@users.noreply.github.com>
2019-06-22 01:38:37 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config(); // this is important!
 module.exports = {
     "development": {
-        "username": process.env.DB_USERNAME,
+      "username": process.env.DB_USERNAME,
         "password": process.env.DB_PASSWORD,
-        "database": "MMM",
-        "host": "localhost",
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
         "dialect": "mysql"
     },
     "test": {
