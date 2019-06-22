68b6413adaf38bbf6b4c4206ff180a25a2d0d099
Fatih Ersoy <f.ersoytx@gmail.com>
2019-06-22 01:01:16 -0500

    database info has been refreshed

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ module.exports = {
     "development": {
         "username": process.env.DB_USERNAME,
         "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_DATABASE,
-        "host": process.env.DB_HOST,
+        "database": "MMM",
+        "host": "localhost",
         "dialect": "mysql"
     },
     "test": {
